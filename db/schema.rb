cd097e0f364865920518c2310eb3ce7c35ed7161
lucas schraier <lucasfschraier@gmail.com>
2020-06-10 16:51:00 -0400

    Redo spellbooks migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_09_04_223630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spellbooks", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "url", null: false
+    t.text "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["url"], name: "index_spellbooks_on_url", unique: true
+  end
 
 end
