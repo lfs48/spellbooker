be4779a43ad63b74de55571b039a961f271da4d1
lucas schraier <lucasfschraier@gmail.com>
2020-06-11 13:21:00 -0400

    Add create and show actions

---
+++ app/controllers/api/spellbooks_controller.rb
@@ -0,0 +1,27 @@
+
+class Api::SpellbooksController < ApplicationController
+
+    def create
+        @spellbook = Spellbook.new(spellbook_params)
+        if @spellbook.save
+            render "api/spellbooks/show"
+        else
+            render json: @spellbook.errors.full_messages, status: 422
+        end
+    end
+
+    def show
+        @spellbook = Spellbook.find_by(id: params[:id])
+        if @spellbook
+            render "api/spellbooks/show"
+        else
+        end
+    end
+
+    private
+
+    def spellbook_params
+        params.require(:spellbook).permit(:name, :url, :desc)
+    end
+
+end
