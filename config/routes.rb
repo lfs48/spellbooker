17a391824729cd21211467e566f5d7895dc985bb
lucas schraier <lucasfschraier@gmail.com>
2020-06-11 13:21:00 -0400

    Add routes for spellbooks (create, update, show) and spells (create, update, show, destroy) under api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   root to: "pages#root"
 
+  namespace :api, defaults: {format: :json} do
+    resources :spellbooks, only: [:create,  :update, :show]
+    resources :spells, only: [:create, :update, :show, :destroy]
+  end
+
 end
