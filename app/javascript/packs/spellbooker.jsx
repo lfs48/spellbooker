bd0ec0496203a11cce873a53794a114fa3286885
lucas schraier <lucasfschraier@gmail.com>
2020-06-14 12:48:00 -0400

    Initialize jquery with rails authentication token

--- app/javascript/packs/spellbooker.jsx
+++ app/javascript/packs/spellbooker.jsx
@@ -3,14 +3,21 @@ import ReactDOM from 'react-dom';
 import configureStore from './store';
 import Root from './components/root';
 
+import * as SpellsAPIUtil from './util/api/spells_api_util';
+import * as SpellActions from './actions/spell_actions';
+
 const cb = () => {
 
     let preloadedState = {};
     const store = configureStore(preloadedState);
 
     const root = document.getElementById("root");
-
+    
     ReactDOM.render(<Root store={store} />, root);
+
+    $.ajaxSetup({
+      headers: { 'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content') }
+    });
 };
 
 document.addEventListener("DOMContentLoaded", cb);
