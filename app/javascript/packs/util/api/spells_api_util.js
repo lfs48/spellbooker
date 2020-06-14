c82cd3c3f3624374a8ed349cf38a5893162e1d23
lucas schraier <lucasfschraier@gmail.com>
2020-06-14 12:48:00 -0400

    Add spells and spellbooks API calls to create and receive spells/spellbooks

---
+++ app/javascript/packs/util/api/spells_api_util.js
@@ -0,0 +1,30 @@
+export const fetchSpellbook = (spellbook) => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/spellbooks/`,
+        data: { spellbook: spellbook }
+    });
+};
+
+export const createSpellbook = (spellbook) => {
+    return $.ajax({
+        method: 'POST',
+        url: `api/spellbooks/`,
+        data: { spellbook: spellbook }
+    });
+};
+
+export const fetchSpell = (id) => {
+    return $.ajax({
+        method: 'GET',
+        url: `api/spells/${id}`
+    });
+};
+
+export const createSpell= (spell) => {
+    return $.ajax({
+        method: 'POST',
+        url: `api/spells/`,
+        data: { spell: spell }
+    });
+};
