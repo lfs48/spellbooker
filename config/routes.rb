6efaea350bbcbcf7f46eb85f15e485336407b2ac
lucas schraier <lucasfschraier@gmail.com>
2020-06-14 12:48:00 -0400

    Change spellbooks routes to be resource rather than resources to remove id dependency in url'

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#root"
 
   namespace :api, defaults: {format: :json} do
-    resources :spellbooks, only: [:create,  :update, :show]
+    resource :spellbooks, only: [:create,  :update, :show]
     resources :spells, only: [:create, :update, :show, :destroy]
   end
 
