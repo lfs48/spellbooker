357a02fbd3da2f2c81aa1ccd9cfa3ea6db6168ff
lucas schraier <lucasfschraier@gmail.com>
2020-06-16 14:33:00 -0400

    Make get request for spell data from dnd5e api

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+spell_list = RestClient.get("https://www.dnd5eapi.co/api/spells")
+spell_arr = JSON.parse(spell_list)["results"]
