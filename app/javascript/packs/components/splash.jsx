acb8866173eab967159b876e9700f4049e474ef0
lucas schraier <lucasfschraier@gmail.com>
2020-06-18 13:07:00 -0400

    Handle clicking spells from list to display info

--- app/javascript/packs/components/splash.jsx
+++ app/javascript/packs/components/splash.jsx
@@ -1,10 +1,12 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {fetchSpellbook} from '../actions/spell_actions'; 
 
 const Splash = () => {
 
     const dispatch = useDispatch();
+
+    const [selectedSpell, setSelectedSpell] = useState(0);
 
     useEffect( () => {
         dispatch(fetchSpellbook({url: "srd"}))
@@ -18,9 +20,13 @@ const Splash = () => {
 
     let spell_lis = <></>;
     spell_lis = spells.slice(0,9).map( (spell, i) => {
-        debugger
-        return <li key={i}>{spell.name}</li>
+        return <li key={i} onClick={e => handleClickSpell(e, i)}>{spell.name}</li>
     });
+
+    const handleClickSpell = (event, id) => {
+        event.preventDefault();
+        setSelectedSpell(id);
+    }
 
     return(
         <section id="spellbook-container">
@@ -31,7 +37,7 @@ const Splash = () => {
            </aside>
            <section id="spell-section-right">
                <header id="spell-filters-bar">placeholder text</header>
-               <article id="selected-spell-info">placeholder text</article>
+                <article id="selected-spell-info">{spells[selectedSpell] ? spells[selectedSpell].name : ""}</article>
            </section>
         </section>
     )
