fe419d177a195efe4879b332e466fa8a8cd3ac24
lucas schraier <lucasfschraier@gmail.com>
2020-06-18 13:07:00 -0400

    Extract spell aray from state and render list of spell names

--- app/javascript/packs/components/splash.jsx
+++ app/javascript/packs/components/splash.jsx
@@ -1,5 +1,5 @@
-import React, {useEffect, useSelector} from 'react';
-import {useDispatch} from 'react-redux';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import {fetchSpellbook} from '../actions/spell_actions'; 
 
 const Splash = () => {
@@ -7,13 +7,28 @@ const Splash = () => {
     const dispatch = useDispatch();
 
     useEffect( () => {
-        dispatch(fetchSpellbook({url: "srd"})),
-        []
+        dispatch(fetchSpellbook({url: "srd"}))
+    }, []);
+
+    const {spells} = useSelector(
+        state => ({
+            spells: Object.values(state.spells)
+        })
+    );
+
+    let spell_lis = <></>;
+    spell_lis = spells.slice(0,9).map( (spell, i) => {
+        debugger
+        return <li key={i}>{spell.name}</li>
     });
 
     return(
         <section id="spellbook-container">
-           <aside id="spell-list-sidebar">placeholder text</aside>
+           <aside id="spell-list-sidebar">
+               <ul>
+                    {spell_lis}
+               </ul>
+           </aside>
            <section id="spell-section-right">
                <header id="spell-filters-bar">placeholder text</header>
                <article id="selected-spell-info">placeholder text</article>
