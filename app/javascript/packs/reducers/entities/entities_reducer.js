0d710dfb35de37d43a11db22ade6e44b3e212a57
lucas schraier <lucasfschraier@gmail.com>
2020-06-19 12:17:00 -0400

    Include dndclasses slice of state

--- app/javascript/packs/reducers/entities/entities_reducer.js
+++ app/javascript/packs/reducers/entities/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import spellsReducer from './spells_reducer';
+import dndclassReducer from './dndclass_reducer'
 
 const rootReducer = combineReducers({
-    spells: spellsReducer
+    spells: spellsReducer,
+    dndclasses: dndclassReducer
 });
 
 export default rootReducer;
