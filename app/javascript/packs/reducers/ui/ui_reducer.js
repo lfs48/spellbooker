8426eccebd9a1e3e7ff7a62c172a84a8aafe11d3
lucas schraier <lucasfschraier@gmail.com>
2020-06-22 13:58:00 -0400

    Include selectedSpell slice of state in UI slice of state

--- app/javascript/packs/reducers/ui/ui_reducer.js
+++ app/javascript/packs/reducers/ui/ui_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import filtersReducer from './filters/filters_reducer';
+import selectedSpellReducer from './selected_spell_reducer';
 
 const uiReducer = combineReducers({
-    filters: filtersReducer
+    filters: filtersReducer,
+    selectedSpell: selectedSpellReducer
 });
 
 export default uiReducer;
