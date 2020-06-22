a9e278f8f60595375f42854fcf5daeaed03cbfcd
lucas schraier <lucasfschraier@gmail.com>
2020-06-22 13:58:00 -0400

    Add SELECT_SPELL action type, clean up formatting

--- app/javascript/packs/actions/types.js
+++ app/javascript/packs/actions/types.js
@@ -1,4 +1,12 @@
-// Spells
+// Entities
 
-export const RECEIVE_SPELL = "RECEIVE_SPELL"
-export const RECEIVE_SPELLBOOK = "RECEIVE_SPELLBOOK"
+    // Spells
+
+    export const RECEIVE_SPELL = "RECEIVE_SPELL";
+    export const RECEIVE_SPELLBOOK = "RECEIVE_SPELLBOOK";
+
+// UI
+
+    // Selected Spell
+
+    export const SELECT_SPELL = "SELECT_SPELL";
