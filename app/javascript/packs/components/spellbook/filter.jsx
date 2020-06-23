c44f477668e92d5c151cf254b9960d591341b721
lucas schraier <lucasfschraier@gmail.com>
2020-06-23 13:41:00 -0400

    Handle filtering by spell level

--- app/javascript/packs/components/spellbook/filter.jsx
+++ app/javascript/packs/components/spellbook/filter.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import {useDispatch, useSelector} from 'react-redux';
+import {useDispatch} from 'react-redux';
 import {dndclassList} from '../../data/dndclasses';
-import { selectClass } from '../../actions/ui/filter_actions';
+import { selectClass, selectLevel } from '../../actions/ui/filter_actions';
+import { intToOrdinal } from '../../util/functions/util_functions';
 
 const Filter = () => {
 
@@ -11,11 +12,22 @@ const Filter = () => {
         return <option key={i} value={dndclass}>{dndclass}</option>
     });
 
+    const levelOptions = [...Array(10).keys()].map( (level, i) => {
+        return <option key={i} value={level}>{intToOrdinal(level)}</option>
+    });
+
     const handleClassSelect = (event) => {
         event.preventDefault();
         let className = event.target.value;
         if (className === "—") {className = null;}
         dispatch( selectClass(className) );
+    };
+
+    const handleLevelSelect = (event) => {
+        event.preventDefault();
+        let level = event.target.value;
+        if (level === "—") {level = null;}
+        dispatch( selectLevel(level) );
     };
 
     return(
@@ -24,6 +36,11 @@ const Filter = () => {
             <select onChange={e => handleClassSelect(e)}>
                 <option value="—">—</option>
                 {classOptions}
+            </select>
+            <label>Level</label>
+            <select onChange={e => handleLevelSelect(e)}>
+                <option value="—">—</option>
+                {levelOptions}
             </select>
         </header>
     )
