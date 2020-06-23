91530d5069f868a1f0daaffffad8c85abde292eb
lucas schraier <lucasfschraier@gmail.com>
2020-06-23 13:41:00 -0400

    Include levelFilter slice of state

--- app/javascript/packs/reducers/ui/filters/filters_reducer.js
+++ app/javascript/packs/reducers/ui/filters/filters_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import classFilterReducer from './class_filter_reducer';
+import levelFilterReducer from './level_filter_reducer';
 
 const filtersReducer = combineReducers({
-    classFilter: classFilterReducer
+    classFilter: classFilterReducer,
+    levelFilter: levelFilterReducer
 });
 
 export default filtersReducer;
---
+++ app/javascript/packs/reducers/ui/filters/level_filter_reducer.js
@@ -0,0 +1,19 @@
+import {merge} from 'lodash';
+import { SELECT_LEVEL, RECEIVE_SPELLBOOK } from '../../../actions/types';
+
+const levelFilterReducer = (state = {}, action) => {
+    const newState = merge({}, state);
+    Object.freeze(state);
+
+    switch (action.type) {
+        default: return state;
+
+        case SELECT_LEVEL:
+            return action.level;
+        
+        case RECEIVE_SPELLBOOK:
+            return null;
+    }
+};
+
+export default levelFilterReducer;
