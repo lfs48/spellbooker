57991a6b5c19c0adbe433c4fb2d0ef636254d15a
lucas schraier <lucasfschraier@gmail.com>
2020-06-23 13:41:00 -0400

    Add selectLevel action to update levelFilter slice of state

--- app/javascript/packs/actions/ui/filter_actions.js
+++ app/javascript/packs/actions/ui/filter_actions.js
@@ -1,8 +1,13 @@
 // Standard actions
 
-import { SELECT_CLASS } from "../types";
+import { SELECT_CLASS, SELECT_LEVEL } from "../types";
 
 export const selectClass = (dndclass) => ({
     type: SELECT_CLASS,
     dndclass: dndclass
+});
+
+export const selectLevel = (level) => ({
+    type: SELECT_LEVEL,
+    level: level
 });
