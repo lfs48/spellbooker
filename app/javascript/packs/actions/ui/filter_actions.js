75dc2b14470ceb3bc818519e7de59b448892026e
lucas schraier <lucasfschraier@gmail.com>
2020-06-24 14:05:00 -0400

    Add selectSchool action

--- app/javascript/packs/actions/ui/filter_actions.js
+++ app/javascript/packs/actions/ui/filter_actions.js
@@ -1,6 +1,6 @@
 // Standard actions
 
-import { SELECT_CLASS, SELECT_LEVEL } from "../types";
+import { SELECT_CLASS, SELECT_LEVEL, SELECT_SCHOOL } from "../types";
 
 export const selectClass = (dndclass) => ({
     type: SELECT_CLASS,
@@ -10,4 +10,9 @@ export const selectClass = (dndclass) => ({
 export const selectLevel = (level) => ({
     type: SELECT_LEVEL,
     level: level
+});
+
+export const selectSchool = (school) => ({
+    type: SELECT_SCHOOL,
+    school: school
 });
