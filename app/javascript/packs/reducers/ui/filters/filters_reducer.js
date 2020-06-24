79810e302c02367445d771a8b509be6076ed4fe9
lucas schraier <lucasfschraier@gmail.com>
2020-06-24 14:05:00 -0400

    Include schoolFilter slice of state

--- app/javascript/packs/reducers/ui/filters/filters_reducer.js
+++ app/javascript/packs/reducers/ui/filters/filters_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import classFilterReducer from './class_filter_reducer';
 import levelFilterReducer from './level_filter_reducer';
+import schoolFilterReducer from './school_filter_reducer';
 
 const filtersReducer = combineReducers({
     classFilter: classFilterReducer,
-    levelFilter: levelFilterReducer
+    levelFilter: levelFilterReducer,
+    schoolFilter: schoolFilterReducer
 });
 
 export default filtersReducer;
