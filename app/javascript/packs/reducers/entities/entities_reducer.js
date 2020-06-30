8a07507b6c01d2c742d24b1cbb81640ae4b5db6d
lucas schraier <lucasfschraier@gmail.com>
2020-06-30 12:13:00 -0400

    Include spellbook slice of state

--- app/javascript/packs/reducers/entities/entities_reducer.js
+++ app/javascript/packs/reducers/entities/entities_reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import spellsReducer from './spells_reducer';
 import dndclassReducer from './dndclass_reducer'
+import spellbookReducer from './spellbook_reducer';
 
 const rootReducer = combineReducers({
     spells: spellsReducer,
+    spellbook: spellbookReducer,
     dndclasses: dndclassReducer
 });
 
