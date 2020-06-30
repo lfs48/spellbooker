f85b2f76dcd6ab19167d138024dfc11f05221203
lucas schraier <lucasfschraier@gmail.com>
2020-06-30 12:13:00 -0400

    Render splash component on / route, spellbook on /spellbook route

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Spellbook from './spellbook/spellbook';
+import Splash from './splash';
 
 const App = () => (
     <main id="app-container">
         <Switch>
-            <Route path="/" component={Spellbook} />
+            <Route path="/spellbook" component={Spellbook} />
+            <Route path="/" component={Splash} />
         </Switch>
     </main>
 );
