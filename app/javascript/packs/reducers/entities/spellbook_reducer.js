536fbd4e5f3c47bdf34948876e6cbce387581462
lucas schraier <lucasfschraier@gmail.com>
2020-06-30 12:13:00 -0400

    Handle RECEIVE_SPELLBOOK action

---
+++ app/javascript/packs/reducers/entities/spellbook_reducer.js
@@ -0,0 +1,20 @@
+import {merge} from 'lodash';
+import { RECEIVE_SPELLBOOK } from '../../actions/types';
+
+const spellbookReducer = (state = {}, action) => {
+    const newState = merge({}, state);
+    Object.freeze(state);
+
+    switch (action.type) {
+        default: return state;
+
+        case RECEIVE_SPELLBOOK:
+            const spellbook = {
+                id: action.spellbook.id,
+                name: action.spellbook.name
+            };
+            return spellbook;
+    }
+};
+
+export default spellbookReducer;
