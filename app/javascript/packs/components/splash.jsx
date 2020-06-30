1cbfe3a1b5403070a7c0faea3a4b1d8d5cb19397
lucas schraier <lucasfschraier@gmail.com>
2020-06-30 12:13:00 -0400

    Render state-controlled input field for spellbook name and button to create spellbook using input name

--- app/javascript/packs/components/splash.jsx
+++ app/javascript/packs/components/splash.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link, useHistory} from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { createSpellbook } from '../actions/entities/spell_actions';
@@ -7,6 +7,8 @@ const Splash = () => {
 
     const history = useHistory();
     const dispatch = useDispatch();
+
+    const [nameInput, setNameInput] = useState("");
 
     const handleSRD = (event) => {
         event.preventDefault();
@@ -23,10 +25,31 @@ const Splash = () => {
         .then( res => history.push(`/spellbook/${res.spellbook.url}`) );
     }
 
+    const updateNameInput = (event) => {
+        event.preventDefault();
+        setNameInput(event.target.value);
+    }
+
+    const handleCreateButton = (event) => {
+        event.preventDefault();
+        const spellbook = { name: nameInput };
+        dispatch( createSpellbook(spellbook) )
+        .then( res => history.push(`/spellbook/${res.spellbook.url}`) );
+    }
+
     return(
         <section id="splash-container">
             <button onClick={e => handleSRD(e)}>View SRD Spells</button>
             <button onClick={e => handleNewSpellbook(e)}>Create New Spellbook</button>
+            <form>
+                <input
+                    type="text"
+                    value={nameInput}
+                    placeholder="Name"
+                    onChange={e => updateNameInput(e)}
+                ></input>
+                <button onClick={e => handleCreateButton(e)}>Create</button>
+            </form>
         </section>
     )
 
