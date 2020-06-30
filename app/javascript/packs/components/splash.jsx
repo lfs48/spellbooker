7037308fe3371f9ffd6a1a6b55c9c6aa3e3b8c6e
lucas schraier <lucasfschraier@gmail.com>
2020-06-30 12:13:00 -0400

    Render buttons to view SRD or create new spell, handled by pushing appropriate spellbook url to browser history

--- app/javascript/packs/components/splash.jsx
+++ app/javascript/packs/components/splash.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
+import {Link, useHistory} from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { createSpellbook } from '../actions/entities/spell_actions';
 
 const Splash = () => {
 
-return(
-        <></>
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const handleSRD = (event) => {
+        event.preventDefault();
+        history.push("/spellbook/srd");
+    }
+
+    const handleNewSpellbook = (event) => {
+        event.preventDefault();
+        const spellbook = {
+            name: "blah",
+            desc: "blah"
+        };
+        dispatch( createSpellbook(spellbook) )
+        .then( res => history.push(`/spellbook/${res.spellbook.url}`) );
+    }
+
+    return(
+        <section id="splash-container">
+            <button onClick={e => handleSRD(e)}>View SRD Spells</button>
+            <button onClick={e => handleNewSpellbook(e)}>Create New Spellbook</button>
+        </section>
     )
 
 }
