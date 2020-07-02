e4901edee410ee5930b17691f46e2146bf7a30bc
lucas schraier <lucasfschraier@gmail.com>
2020-07-02 15:37:00 -0400

    Render additional spell information in dl's

--- app/javascript/packs/components/spellbook/spell.jsx
+++ app/javascript/packs/components/spellbook/spell.jsx
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
 import {intToOrdinal} from '../../util/functions/util_functions'
 import {dndclassList} from '../../data/dnd_data';
+import { selectSpell } from '../../actions/ui/selected_spell_actions';
 
 const Spell = () => {
 
@@ -11,7 +12,7 @@ const Spell = () => {
 
     const {selectedSpell} = useSelector(
         state => ({
-            selectedSpell: state.entities.spells[state.ui.selectedSpell] ? state.entities.spells[state.ui.selectedSpell] : {name:"", range: "", level: "", components:"", material_desc:"", ritual: false, conc: false, duration: "", cast_time: "", school: "", classes: "", desc: [], higher_level_desc: [], notes: ""}
+            selectedSpell: state.entities.spells[state.ui.selectedSpell] ? state.entities.spells[state.ui.selectedSpell] : {name:"", range: "", level: "", components:"", material:"", ritual: false, concentration: false, duration: "", casting_time: "", school: "", classes: "", desc: [], higher_level_desc: [], notes: ""}
         })
     );
     
@@ -20,18 +21,26 @@ const Spell = () => {
             <h1>{selectedSpell.name}</h1>
 
             <section id="spell-details">
-                <span>{intToOrdinal(selectedSpell.level)}-level {selectedSpell.school}</span>
+                <span>{intToOrdinal(selectedSpell.level)}-level {selectedSpell.school} {selectedSpell.ritual? "(ritual)" : ""}</span>
+                <dl>
+                    <dt>Classes</dt>
+                    <dd>: {selectedSpell.classes.split(",").join(", ")}</dd>
+                </dl>
+                <dl>
+                    <dt>Casting Time</dt>
+                    <dd>: {selectedSpell.casting_time}</dd>
+                </dl>
                 <dl>
                     <dt>Range</dt>
                     <dd>: {selectedSpell.range}</dd>
                 </dl>
                 <dl>
                     <dt>Components</dt>
-                    <dd>: {selectedSpell.components}</dd>
+                    <dd>: {selectedSpell.components} {selectedSpell.material ? `(${selectedSpell.material})` : ""}</dd>
                 </dl>
                 <dl>
                     <dt>Duration</dt>
-                    <dd>: {selectedSpell.duration}</dd>
+                    <dd>:{selectedSpell.duration} {selectedSpell.concentration ? "(concentration)" : ""}</dd>
                 </dl>
 
             </section>
