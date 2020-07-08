ffb1435732e3d81e2952c2d4124cdbb78ccfa6a3
lucas schraier <lucasfschraier@gmail.com>
2020-07-08 12:59:00 -0400

    Conditionally render component based on modal state in redux store

--- app/javascript/packs/components/modal.jsx
+++ app/javascript/packs/components/modal.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import SpellForm from './spellbook/spell_form';
 
 const Modal = () => {
+
+    const modalComponents = ({
+        SpellForm: <SpellForm />
+    });
+
+    const {componentName} = useSelector(
+        state => ({
+            componentName: state.ui.modal,
+        })
+    );
+
+    const component = modalComponents[componentName] ? modalComponents[componentName] : <></>;
+
     return(
-        <></>
+        <div className="modal-bg">
+            {component}
+        </div>
     );
 };
 
-export const Modal;
+export default Modal;
