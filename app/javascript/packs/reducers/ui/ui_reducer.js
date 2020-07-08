ceae1bd138be48816727796e5ac7899da11056bd
lucas schraier <lucasfschraier@gmail.com>
2020-07-08 12:59:00 -0400

    Include modal slice of state

--- app/javascript/packs/reducers/ui/ui_reducer.js
+++ app/javascript/packs/reducers/ui/ui_reducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import filtersReducer from './filters/filters_reducer';
 import openSpellsReducer from './open_spells_reducer';
 import focusSpellReducer from './focus_spell_reducer';
+import modalReducer from './modal_reducer';
 
 const uiReducer = combineReducers({
     filters: filtersReducer,
     openSpells: openSpellsReducer,
-    focusSpell: focusSpellReducer
+    focusSpell: focusSpellReducer,
+    modal: modalReducer
 });
 
 export default uiReducer;
