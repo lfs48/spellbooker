c3796bd0cb1b5384f23ea976229a6e31de54c9a0
lucas schraier <lucasfschraier@gmail.com>
2020-07-08 12:59:00 -0400

    Render Modal component at top level

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Spellbook from './spellbook/spellbook';
 import Splash from './splash';
+import Modal from './modal';
 
 const App = () => (
     <main id="app-container">
+        <Modal />
         <Switch>
             <Route path="/spellbook" component={Spellbook} />
             <Route path="/" component={Splash} />
