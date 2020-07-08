8e81efe0b5fe34977a8646174eaff162c3dd30c0
lucas schraier <lucasfschraier@gmail.com>
2020-07-08 12:59:00 -0400

    Handle create button by dispatching action to open spell form modal

--- app/javascript/packs/components/spellbook/spellbook_menu.jsx
+++ app/javascript/packs/components/spellbook/spellbook_menu.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { useDispatch } from 'react-redux';
+import { openModal } from '../../actions/ui/modal_actions';
 
 const SpellbookMenu = () => {
+
+    const dispatch = useDispatch();
+
+    const handleCreateButton = (event) => {
+        event.preventDefault();
+        dispatch( openModal("SpellForm") );
+    }
+
+
     return(
         <aside>
-            <button>+</button>
+            <button onClick={e => handleCreateButton(e)}>+</button>
         </aside>
     );
 };
