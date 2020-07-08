6411e431ecc907fa4941bdbeb9673b3508fbd217
lucas schraier <lucasfschraier@gmail.com>
2020-07-08 12:59:00 -0400

    Handle OPEN_MODAL and CLOSE_MODAL actions

---
+++ app/javascript/packs/reducers/ui/modal_reducer.js
@@ -0,0 +1,19 @@
+import {merge} from 'lodash';
+import { OPEN_MODAL, CLOSE_MODAL } from '../../actions/types';
+
+const modalReducer = (state = {}, action) => {
+    const newState = merge([], state);
+    Object.freeze(state);
+
+    switch (action.type) {
+        default: return state;
+
+        case OPEN_MODAL:
+            return action.modal;
+        
+        case CLOSE_MODAL:
+            return null;
+    }
+};
+
+export default modalReducer;
