4bfd9e70b4ef004652c2cdfb192b9096791f1066
lucas schraier <lucasfschraier@gmail.com>
2020-07-14 17:39:00 -0400

    Include editSpell slice of state

--- app/javascript/packs/reducers/ui/ui_reducer.js
+++ app/javascript/packs/reducers/ui/ui_reducer.js
@@ -3,12 +3,14 @@ import filtersReducer from './filters/filters_reducer';
 import openSpellsReducer from './open_spells_reducer';
 import focusSpellReducer from './focus_spell_reducer';
 import modalReducer from './modal_reducer';
+import editSpellReducer from './edit_spell_reducer';
 
 const uiReducer = combineReducers({
     filters: filtersReducer,
     openSpells: openSpellsReducer,
     focusSpell: focusSpellReducer,
-    modal: modalReducer
+    modal: modalReducer,
+    editSpell: editSpellReducer
 });
 
 export default uiReducer;
