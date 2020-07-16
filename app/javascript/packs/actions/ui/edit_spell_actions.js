0ebe9682ebfef36aec07ef60ea68be28d00da059
lucas schraier <lucasfschraier@gmail.com>
2020-07-16 14:11:00 -0400

    Remove editSpell slice of state, now handled by data key in modal state

--- app/javascript/packs/actions/ui/edit_spell_actions.js
+++
@@ -1,6 +0,0 @@
-import { EDIT_SPELL } from "../types"
-
-export const editSpell = (id) => ({
-    type: EDIT_SPELL,
-    id: id
-});
--- app/javascript/packs/reducers/ui/ui_reducer.js
+++ app/javascript/packs/reducers/ui/ui_reducer.js
@@ -3,14 +3,12 @@ import filtersReducer from './filters/filters_reducer';
 import openSpellsReducer from './open_spells_reducer';
 import focusSpellReducer from './focus_spell_reducer';
 import modalReducer from './modal_reducer';
-import editSpellReducer from './edit_spell_reducer';
 
 const uiReducer = combineReducers({
     filters: filtersReducer,
     openSpells: openSpellsReducer,
     focusSpell: focusSpellReducer,
-    modal: modalReducer,
-    editSpell: editSpellReducer
+    modal: modalReducer
 });
 
 export default uiReducer;
