ba4ef9a4461149a27e03da8dca37c131417a0cc5
lucas schraier <lucasfschraier@gmail.com>
2020-07-16 14:11:00 -0400

    Handle confirm button by dispatching action to update spellbook with spell removed from spells list, then closing modal and spell panel

--- app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
+++ app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { closeModal } from '../../actions/ui/modal_actions';
+import {merge} from 'lodash';
+import { updateSpellbook } from '../../actions/entities/spell_actions';
+import { closeSpell } from '../../actions/ui/selected_spell_actions';
 
 const DeleteSpellConfirmation = () => {
 
     const dispatch = useDispatch();
 
+    const {spells, spellbook, modal} = useSelector(
+        state => ({
+            spells: state.entities.spells,
+            spellbook: state.entities.spellbook,
+            modal: state.ui.modal
+        })
+    )
+
     const handleCancel = (event) => {
         event.preventDefault();
+        dispatch( closeModal() );
+    }
+
+    const handleConfirm = (event) => {
+        event.preventDefault();
+        const newSpells = merge({}, spells);
+        delete newSpells[modal.data.id];
+        const newSpellbook = merge({}, spellbook);
+        newSpellbook.spells = JSON.stringify(newSpells);
+        dispatch( closeSpell(modal.data.id) )
+        dispatch( updateSpellbook(newSpellbook) );
         dispatch( closeModal() );
     }
 
@@ -15,7 +37,7 @@ const DeleteSpellConfirmation = () => {
         <form>
             <h1>Delete Spell?</h1>
             <button onClick={e => handleCancel(e)}>Cancel</button>
-            <button>Confirm</button>
+            <button onClick={e => handleConfirm(e)}>Confirm</button>
         </form>
     );
 };
