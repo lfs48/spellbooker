f44b4c7f6c3c6446302108937eae88dd1f1a5af8
lucas schraier <lucasfschraier@gmail.com>
2020-07-16 14:11:00 -0400

    Remove unused imports

--- app/javascript/packs/components/spellbook/spellbook_menu.jsx
+++ app/javascript/packs/components/spellbook/spellbook_menu.jsx
@@ -3,12 +3,10 @@ import { useDispatch } from 'react-redux';
 import { openModal } from '../../actions/ui/modal_actions';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
-import { useLocation } from 'react-router-dom';
 
 const SpellbookMenu = () => {
 
     const dispatch = useDispatch();
-    const location = useLocation();
 
     const handleCreateButton = (event) => {
         event.preventDefault();
