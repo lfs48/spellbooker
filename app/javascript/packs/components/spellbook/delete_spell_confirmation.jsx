d599e71a47f36215987623253c521042d1ae6620
lucas schraier <lucasfschraier@gmail.com>
2020-07-16 14:11:00 -0400

    Render buttons, handle delete button by dispatching closeModal action

--- app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
+++ app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../actions/ui/modal_actions';
 
 const DeleteSpellConfirmation = () => {
+
+    const dispatch = useDispatch();
+
+    const handleCancel = (event) => {
+        event.preventDefault();
+        dispatch( closeModal() );
+    }
+
     return(
         <form>
             <h1>Delete Spell?</h1>
+            <button onClick={e => handleCancel(e)}>Cancel</button>
+            <button>Confirm</button>
         </form>
     );
 };
