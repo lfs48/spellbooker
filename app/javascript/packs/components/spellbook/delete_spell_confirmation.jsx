1dc0460e47013b9f0fcfe592797359af93fdd650
lucas schraier <lucasfschraier@gmail.com>
2020-07-16 14:11:00 -0400

    Initial commit with component skeleton

---
+++ app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeleteSpellConfirmation = () => {
+    return(
+        <form>
+            <h1>Delete Spell?</h1>
+        </form>
+    );
+};
+
+export default DeleteSpellConfirmation;
