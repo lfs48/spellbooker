b49395d6e6beb2d8f665b8111c530c0e16e12654
lucas schraier <lucasfschraier@gmail.com>
2020-07-20 13:08:00 -0400

    Render controlled inputs for name and spells

--- app/javascript/packs/components/spellbook/create_class.jsx
+++ app/javascript/packs/components/spellbook/create_class.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { useState } from 'react';
+import {merge} from 'lodash';
 
 const CreateClass = () => {
+
+    const [inputs, setInputs] = useState({
+        name: "",
+        spells: ""
+    });
+
+    const handleInput = (event, field) => {
+        event.preventDefault();
+        newState = merge({}, inputs);
+        newState[field] = event.target.value;
+        setInputs(newState);
+    }
+
     return(
         <form>
-
+            <input
+                type="text"
+                placeholder="Name"
+                value={inputs.name}
+                onChange={e => handleInput(e, 'name')}
+            ></input>
+            <label>Optional: Seed class spell list with a comma separated list of spell names</label>
+            <textarea
+                type="text"
+                placeholder="Acid Arrow, Aid, Animate Objects, Awaken, ..."
+                value={inputs.spells}
+                onChange={e => handleInput(e, 'spells')}
+            ></textarea>
         </form>
     )
 };
