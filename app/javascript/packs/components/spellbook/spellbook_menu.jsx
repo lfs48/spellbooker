5a5ba4c36d203354e2f307c57e6eb1e92e38f1f0
lucas schraier <lucasfschraier@gmail.com>
2020-07-20 13:08:00 -0400

    Render add class button

--- app/javascript/packs/components/spellbook/spellbook_menu.jsx
+++ app/javascript/packs/components/spellbook/spellbook_menu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { openModal } from '../../actions/ui/modal_actions';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons'
+import { faPlusSquare, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const SpellbookMenu = () => {
 
@@ -13,10 +13,14 @@ const SpellbookMenu = () => {
         dispatch( openModal("SpellForm") );
     }
 
+    const handleAddClassButton = (event) => {
+        event.preventDefault();
+    }
 
     return(
         <aside id="spell-menu">
             <FontAwesomeIcon icon={faPlusSquare} onClick={e => handleCreateButton(e) }/>
+            <FontAwesomeIcon icon={faUserPlus} onClick={e => handleAddClassButton(e)} />
         </aside>
     );
 };
