8d2dcb1a9f9ed6dc3e4ba6bd33d8d0b984ad6706
lucas schraier <lucasfschraier@gmail.com>
2020-07-20 13:08:00 -0400

    Style menu

--- app/javascript/packs/components/spellbook/spellbook_menu.jsx
+++ app/javascript/packs/components/spellbook/spellbook_menu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch } from 'react-redux';
 import { openModal } from '../../actions/ui/modal_actions';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import { faPlusSquare, faUserPlus } from '@fortawesome/free-solid-svg-icons'
+import { faScroll, faUserPlus } from '@fortawesome/free-solid-svg-icons'
 
 const SpellbookMenu = () => {
 
@@ -20,8 +20,14 @@ const SpellbookMenu = () => {
 
     return(
         <aside id="spell-menu">
-            <FontAwesomeIcon icon={faPlusSquare} onClick={e => handleCreateButton(e) }/>
-            <FontAwesomeIcon icon={faUserPlus} onClick={e => handleAddClassButton(e)} />
+            <section className="spell-menu-item" onClick={e => handleCreateButton(e) }>
+                <label>Create Spell</label>
+                <FontAwesomeIcon icon={faScroll}/>
+            </section>
+            <section className="spell-menu-item" onClick={e => handleAddClassButton(e)}>
+                <label>Create Class</label>
+                <FontAwesomeIcon icon={faUserPlus}/>
+            </section>
         </aside>
     );
 };
