98c8ea2179e1c678a93d13e8268cedb58406923b
lucas schraier <lucasfschraier@gmail.com>
2020-07-20 13:08:00 -0400

    Remove class data, now stored directly in individual spellbooks

--- app/javascript/packs/data/dnd_data.js
+++ app/javascript/packs/data/dnd_data.js
@@ -1,2 +1,1 @@
-export const dndclassList = ["bard", "cleric", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"];
 export const dndSchoolList = ["abjuration", "conjuration", "divination", "enchantment", "evocation", "illusion", "necromancy", "transmutation"];
