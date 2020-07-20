9296f4c8ff4525493c0ff434d24e1e2c8bb4957c
lucas schraier <lucasfschraier@gmail.com>
2020-07-20 13:08:00 -0400

    Add initialized_data method which calls other methods to set initial data of spellbook

--- app/models/spellbook.rb
+++ app/models/spellbook.rb
@@ -12,12 +12,22 @@ class Spellbook < ApplicationRecord
         return url
     end
 
+    def initialize_data
+        self.ensure_url
+        self.populate_initial_spells
+        self.populate_initial_classes
+    end
+
     def ensure_url
         self.url ||= Spellbook.generate_url
     end
 
     def populate_initial_spells
         self.spells = Spellbook.first.spells.clone
+    end
+
+    def populate_initial_classes
+        self.classes = Spellbook.first.classes.clone
     end
 
 end
