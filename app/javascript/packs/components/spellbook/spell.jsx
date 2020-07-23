315870a014de701187dbef7040277a3f77318179
lucas schraier <lucasfschraier@gmail.com>
2020-07-23 13:59:00 -0400

    Remove references to spellbook name header in setting initial top position

--- app/javascript/packs/components/spellbook/spell.jsx
+++ app/javascript/packs/components/spellbook/spell.jsx
@@ -17,7 +17,7 @@ const Spell = (props) => {
 
     const [styleData, setStyleData] = useState({
         left: Math.floor( Math.random() * (window.innerWidth - $(`#spell-list-sidebar`).outerWidth(true) - 500 ) ) + $(`#spell-list-sidebar`).outerWidth(true) - 100,
-        top: Math.floor( Math.random() * (window.innerHeight - $(`#spellbook-name-header`).outerHeight(true) - $(`#filters-nav`).outerHeight(true) - 400 ) ) + $(`#spellbook-name-header`).outerHeight(true) + $(`#filters-nav`).outerHeight(true),
+        top: Math.floor( Math.random() * (window.innerHeight - $(`#filters-nav`).outerHeight(true) - 400 ) ) + $(`#filters-nav`).outerHeight(true),
         width: 500,
         height: 400,
         minHeight: 50,
