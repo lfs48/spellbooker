7cb71208f4297d6e38125feecb4aad879f274c64
lucas schraier <lucasfschraier@gmail.com>
2020-07-27 17:01:00 -0400

    Wrapp app in cookie provider

--- app/javascript/packs/components/root.jsx
+++ app/javascript/packs/components/root.jsx
@@ -2,12 +2,15 @@
 import App from './App';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import { CookiesProvider } from 'react-cookie';
 
 const Root = ({store}) => (
     <Provider store={store}>
-        <HashRouter>
-            <App />
-        </HashRouter>
+        <CookiesProvider>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </CookiesProvider>
     </Provider>
 );
 
