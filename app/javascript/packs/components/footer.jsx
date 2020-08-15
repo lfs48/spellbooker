acc749c8d2cc3181608bc6f821313819cdaf786d
lucas schraier <lucasfschraier@gmail.com>
2020-08-15 19:13:00 -0400

    Render github, patreon, and contact buttons

--- app/javascript/packs/components/footer.jsx
+++ app/javascript/packs/components/footer.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faQuestionCircle} from '@fortawesome/free-solid-svg-icons';
+import {faQuestionCircle, faEnvelope} from '@fortawesome/free-solid-svg-icons';
+import {faGithub, faPatreon} from '@fortawesome/free-brands-svg-icons';
 
 const Footer = () => {
     return(
         <footer id="app-footer">
-            <FontAwesomeIcon id="splash-info-button" icon={faQuestionCircle} />
+            <section>
+                <FontAwesomeIcon id="splash-contact-button" icon={faEnvelope} />
+                <label>Contact</label>
+            </section>
+            <section>
+                <FontAwesomeIcon id="splash-patreon-button" icon={faPatreon} />
+                <label>Patreon</label>
+            </section>
+            <section>
+                <FontAwesomeIcon id="splash-github-button" icon={faGithub} />
+                <label>Github</label>
+            </section>
+            <section>
+                <FontAwesomeIcon id="splash-info-button" icon={faQuestionCircle} />
+                <label>About</label>
+            </section>
         </footer>
     );
 };
