f8c8b275692838a41c56d7f70d0d24c074d12c8c
lucas schraier <lucasfschraier@gmail.com>
2020-08-16 17:37:00 -0400

    Add about route

--- app/javascript/packs/components/App.jsx
+++ app/javascript/packs/components/App.jsx
@@ -4,6 +4,7 @@ import Spellbook from './spellbook/spellbook';
 import Splash from './splash';
 import Modal from './modal/modal';
 import Footer from './footer';
+import About from './about';
 
 const App = () => (
     <main id="app-container">
@@ -11,6 +12,7 @@ const App = () => (
         <Switch>
             <Route path="/spellbook/edit/:url" component={Spellbook} />
             <Route path="/spellbook/view/:url" component={Spellbook} />
+            <Route path="/about" component={About} />
             <Route exact path="/spellbook">
                 <Redirect to="/spellbook/srd"/>
             </Route>
