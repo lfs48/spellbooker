94b20fae71630dd759e6e4264956a49c152bdf08
lucas schraier <lucasfschraier@gmail.com>
2020-08-17 12:19:00 -0400

    Render contact form with subject select, textarea message input, and cancel/submit buttons

---
+++ app/javascript/packs/components/modal/contact.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { closeModal } from '../../actions/ui/modal_actions';
+
+const Contact = () => {
+
+    const dispatch = useDispatch();
+
+    const contactOptions = ["Report Issue", "Suggest Feature", "Feedback", "Other"].map( (el, i) => {
+        return <option key={i} value={el}>{el}</option>
+    })
+
+    const handleClose = (event) => {
+        event.preventDefault();
+        dispatch( closeModal() );
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
+    return(
+        <form id="contact-form" className="modal-form">
+            <h1>Contact</h1>
+            <section>
+                <label>Subject</label>
+                <select>
+                    {contactOptions}
+                </select>
+            </section>
+            <textarea
+                placeholder="Write a message here"
+            ></textarea>
+            <section>
+                <button onClick={e => handleClose(e)}>Close</button>
+                <button onClick={e => handleSubmit(e)}>Submit</button>
+            </section>
+        </form>
+    );
+}
+
+export default Contact;
