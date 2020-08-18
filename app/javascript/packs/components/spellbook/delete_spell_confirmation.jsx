b4cc454e859035ef87c88213c20ee489cee9af71
lucas schraier <lucasfschraier@gmail.com>
2020-08-18 13:05:00 -0400

    Update to use edit_url from location

--- app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
+++ app/javascript/packs/components/spellbook/delete_spell_confirmation.jsx
@@ -4,15 +4,16 @@ import { closeModal } from '../../actions/ui/modal_actions';
 import {merge} from 'lodash';
 import { updateSpellbook } from '../../actions/entities/spell_actions';
 import { closeSpell } from '../../actions/ui/selected_spell_actions';
+import { useLocation } from 'react-router-dom';
 
 const DeleteSpellConfirmation = () => {
 
     const dispatch = useDispatch();
+    const location = useLocation().pathname;
 
-    const {spells, spellbook, modal} = useSelector(
+    const {spells, modal} = useSelector(
         state => ({
             spells: state.entities.spells,
-            spellbook: state.entities.spellbook,
             modal: state.ui.modal
         })
     )
@@ -26,7 +27,8 @@ const DeleteSpellConfirmation = () => {
         event.preventDefault();
         const newSpells = merge({}, spells);
         delete newSpells[modal.data.id];
-        const newSpellbook = merge({}, spellbook);
+        const newSpellbook = {};
+        newSpellbook.url = location.slice( location.indexOf("edit/") + 5 );
         newSpellbook.spells = JSON.stringify(newSpells);
         dispatch( closeSpell(modal.data.id) )
         dispatch( updateSpellbook(newSpellbook) );
