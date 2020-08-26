c0b8129823c09041fc2dd83c277b283e4c4b7520
lucas schraier <lucasfschraier@gmail.com>
2020-08-26 14:28:00 -0400

    Implement state controlled inputs

--- app/javascript/packs/components/modal/contact.jsx
+++ app/javascript/packs/components/modal/contact.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { closeModal } from '../../actions/ui/modal_actions';
+import {merge} from 'lodash';
 
 const Contact = () => {
 
     const dispatch = useDispatch();
+
+    const [inputs, setInputs] = useState({
+        subject: "Report Issue",
+        body: ""
+    });
 
     const contactOptions = ["Report Issue", "Suggest Feature", "Feedback", "Other"].map( (el, i) => {
         return <option key={i} value={el}>{el}</option>
@@ -19,17 +25,30 @@ const Contact = () => {
         event.preventDefault();
     }
 
+    const handleInput = (event, field) => {
+        event.preventDefault();
+        const newState = merge({}, inputs);
+        newState[field] = event.target.value;
+        setInputs(newState);
+    }
+
     return(
         <form id="contact-form" className="modal-form">
             <h1>Contact</h1>
             <section>
                 <label>Subject</label>
-                <select>
+                <select
+                    value={inputs.subject}
+                    onChange={e => handleInput(e, 'subject')}
+                >
                     {contactOptions}
                 </select>
             </section>
             <textarea
+                value={inputs.body}
+                onChange={e => handleInput(e, 'body')}
                 placeholder="Write a message here"
+                maxLength={1000}
             ></textarea>
             <section>
                 <button onClick={e => handleClose(e)}>Cancel</button>
